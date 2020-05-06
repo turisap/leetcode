0864d2211387254dfec3d1b1dc30fd01a7c714a1
turisap <kirillshakirov57@gmail.com>
2020-05-07 01:19:33 +0300

    random and 2-order sorting

--- medium/next_bigger_number.js
+++ medium/next_bigger_number.js
@@ -1,30 +1,60 @@
 const next_bigger_number = (number) => {
   const digits = number.toString().split("");
-  let nextBig = Number.MAX_SAFE_INTEGER;
+  let inaAsc = true;
+  let possiblySorted = parseInt([...digits].sort().reverse().join(""));
 
-  // idea is to loop over each one and compare with any other one
-  for (let i = digits.length - 1; i >= 0; i--) {
-    for (let j = digits.length - 1; j >= 0; j--) {
-      const copy = [...digits];
-      const current = parseInt(digits[i]);
-      const next = parseInt(digits[j]);
-      const temp = current;
+  if (possiblySorted === number) return -1;
 
-      copy[i] = next;
-      copy[j] = temp;
+  if (digits.length === 2) {
+    return parseInt(digits.reverse().join(""));
+  }
+  // for (let c = 0; c < digits.length - 2; c++) {
+  //   if (parseInt(digits[c]) >= digits[c + 1]) {
+  //     inaAsc = false;
+  //   }
+  // }
 
-      const n = parseInt(copy.join(""));
+  // if (inaAsc) {
+  //   const lastI = digits.length - 1;
+  //   const temp = digits[lastI - 1];
+  //   digits[lastI - 1] = digits[lastI];
+  //   digits[lastI] = temp;
 
-      if (n < nextBig && n > number) {
-        nextBig = n;
-      }
+  //   return parseInt(digits.join(""));
+  // }
+
+  let nextBig;
+  let stopIndex = 0;
+
+  for (let i = digits.length - 2; i >= 0; i--) {
+    if (digits[i] < digits[i + 1]) {
+      stopIndex = i;
+      break;
     }
   }
 
-  return nextBig;
+  let smallestIndex = digits.length - 1;
+  let smallestToRight = digits[smallestIndex];
+
+  for (let j = digits.length - 1; j > stopIndex; j--) {
+    const current = parseInt(digits[j]);
+
+    if (current < smallestToRight) {
+      smallestToRight = current;
+      smallestIndex = j;
+    }
+  }
+
+  const temp = digits[smallestIndex];
+  digits[smallestIndex] = digits[stopIndex];
+  digits[stopIndex] = temp;
+
+  const leftArr = digits.slice(0, stopIndex);
+  const rightArr = digits.slice(stopIndex).sort();
+
+  const res = parseInt(leftArr.concat(rightArr).join(""));
+
+  return res;
 };
 
-// console.log(next_bigger_number(144));
-// console.log(next_bigger_number(123456));
-// console.log(next_bigger_number(67980));
-console.log(next_bigger_number(116398)); // expect 116839
+console.log(next_bigger_number(12));
