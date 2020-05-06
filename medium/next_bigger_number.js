d3264deb8832895adb1b47c2aaf1da557d5ba994
turisap <kirillshakirov57@gmail.com>
2020-05-06 23:46:17 +0300

    flawed number shifter

--- medium/next_bigger_number.js
+++ medium/next_bigger_number.js
@@ -1,28 +1,30 @@
 const next_bigger_number = (number) => {
   const digits = number.toString().split("");
-  let swapped = false;
+  let nextBig = Number.MAX_SAFE_INTEGER;
 
+  // idea is to loop over each one and compare with any other one
   for (let i = digits.length - 1; i >= 0; i--) {
-    for (let j = i - 1; j >= 0; j--) {
+    for (let j = digits.length - 1; j >= 0; j--) {
+      const copy = [...digits];
       const current = parseInt(digits[i]);
       const next = parseInt(digits[j]);
+      const temp = current;
 
-      console.log(i, j);
-      if (current > next) {
-        const temp = current;
+      copy[i] = next;
+      copy[j] = temp;
 
-        digits[i] = next;
-        digits[j] = temp;
-        swapped = true;
-        break;
+      const n = parseInt(copy.join(""));
+
+      if (n < nextBig && n > number) {
+        nextBig = n;
       }
     }
-
-    if (swapped) break;
   }
 
-  return swapped ? parseInt(digits.join("")) : -1;
+  return nextBig;
 };
 
-console.log(next_bigger_number(144));
+// console.log(next_bigger_number(144));
 // console.log(next_bigger_number(123456));
+// console.log(next_bigger_number(67980));
+console.log(next_bigger_number(116398)); // expect 116839
