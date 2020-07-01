50ccff236531f8ec57400181ddd648de92d452fa
turisap <kirillshakirov57@gmail.com>
2020-07-02 00:41:57 +0300

    add prime numbers check

---
+++ interview/prime_number.js
@@ -0,0 +1,13 @@
+function isPrime(n) {
+  if (n === 2) return false;
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+module.exports = isPrime;
---
+++ tests/prime_number.test.js
@@ -0,0 +1,25 @@
+const is_prime = require("../interview/prime_number.js");
+
+describe("should validate prime number", () => {
+  test("should return true for 3", () => {
+    expect(is_prime(3)).toBe(true);
+  });
+  test("should return true for 5", () => {
+    expect(is_prime(5)).toBe(true);
+  });
+  test("should return true for 7", () => {
+    expect(is_prime(7)).toBe(true);
+  });
+  test("should return true for 11", () => {
+    expect(is_prime(11)).toBe(true);
+  });
+  test("should return false for 2", () => {
+    expect(is_prime(2)).toBe(false);
+  });
+  test("should return false for 6", () => {
+    expect(is_prime(6)).toBe(false);
+  });
+  test("should return false for 99", () => {
+    expect(is_prime(99)).toBe(false);
+  });
+});
