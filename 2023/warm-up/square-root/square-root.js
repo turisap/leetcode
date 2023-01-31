c697437d5f63dbbbe9d7a331d5c1cbbd38f7fd62
Kirill Shakirov <kirillshakirov57@gmail.com>
2023-01-31 18:36:11 +0400

    task(easy): square root

---
+++ 2023/warm-up/square-root/square-root.js
@@ -0,0 +1,29 @@
+const squareRoot = (num) => {
+  if (num < 2) {
+    return num;
+  }
+
+  let left = 2;
+  let right = Math.floor(num / 2);
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+    const res = middle * middle;
+
+    if (res === num) {
+      return middle;
+    }
+
+    if (res < num) {
+      left = middle + 1;
+    }
+
+    if (res > num) {
+      right = middle - 1;
+    }
+  }
+
+  return Math.floor(right);
+};
+
+module.exports = squareRoot;
