c91738d74c18feeec2b6e60db2caedc513f73987
Kirill Shakirov <kirillshakirov57@gmail.com>
2023-02-15 15:24:49 +0400

    task: add symmetric tree recursive

--- 2023/warm-up/symmetric-tree/symmetric-tree.js
+++ 2023/warm-up/symmetric-tree/symmetric-tree.js
@@ -14,4 +14,31 @@ const isSymmetricRecursive = function (root) {
   return isSame(root.left, root.right);
 };
 
-module.exports = isSymmetricRecursive;
+const isSymmetricTreeIterative = function (root) {
+  const queue = [root.left, root.right];
+
+  while (queue.length) {
+    const left = queue.shift();
+    const right = queue.pop();
+
+    if (!left && !right) continue;
+
+    if (!left && right) return false;
+
+    if (left && !right) return false;
+
+    if (left.val !== right.val) return false;
+
+    queue.unshift(left.left ?? null);
+
+    queue.push(right.right ?? null);
+
+    queue.unshift(left.right ?? null);
+
+    queue.push(right.left ?? null);
+  }
+
+  return !queue.length;
+};
+
+module.exports = isSymmetricTreeIterative;
--- 2023/warm-up/symmetric-tree/test.spec.js
+++ 2023/warm-up/symmetric-tree/test.spec.js
@@ -47,16 +47,43 @@ const shallowTree = {
     val: 3,
   },
 };
+
+//[2,3,3,4,5,null,4]
+const failedTree = {
+  val: 2,
+  left: {
+    val: 3,
+    left: {
+      val: 4,
+    },
+    right: {
+      val: 5,
+    },
+  },
+  right: {
+    val: 3,
+    left: {
+      val: null,
+    },
+    right: {
+      val: 4,
+    },
+  },
+};
 describe("verifies a symmetric tree", () => {
-  it("returns true", () => {
+  it("verifies a symmetric tree", () => {
     expect(isSymmetric(symTree)).toBe(true);
   });
 
-  it("returns false", () => {
+  it("returns verifies an asymmetric tree", () => {
     expect(isSymmetric(assymTree)).toBe(false);
   });
 
-  it("returns false", () => {
+  it("verifies a shallow tree", () => {
     expect(isSymmetric(shallowTree)).toBe(false);
+  });
+
+  it("verifies an edge case", () => {
+    expect(isSymmetric(failedTree)).toBe(false);
   });
 });
