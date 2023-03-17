c0229823ac61b50ff95362643ce76458e51be58c
Kirill Shakirov <kirillshakirov57@gmail.com>
2023-03-17 13:30:30 +0530

    task: iterative min depth

--- 2023/trees/min-depth/min-depth.js
+++ 2023/trees/min-depth/min-depth.js
@@ -1,17 +1,39 @@
-const minTreeDepth = (root) => {
+const minTreeDepthRecur = (root) => {
   if (!root) return 0;
 
   if (!root.left && !root.right) return 1;
 
   if (root.left && !root.right) {
-    return 1 + minTreeDepth(root.left);
+    return 1 + minTreeDepthRecur(root.left);
   }
 
   if (!root.left && root.right) {
-    return 1 + minTreeDepth(root.right);
+    return 1 + minTreeDepthRecur(root.right);
   }
 
-  return 1 + Math.min(minTreeDepth(root.right), minTreeDepth(root.left));
+  return (
+    1 + Math.min(minTreeDepthRecur(root.right), minTreeDepthRecur(root.left))
+  );
 };
 
-module.exports = minTreeDepth;
+const minDepth = (root) => {
+  if (!root) return 0;
+
+  const stack = [[root, 1]];
+  const minDepth = Number.MAX_SAFE_INTEGER;
+
+  while (stack.length) {
+    const [node, depth] = stack.pop();
+
+    if (!node.right && !node.left) {
+      return Math.min(minDepth, depth);
+    }
+
+    if (node.right) stack.push([node.right, depth + 1]);
+    if (node.left) stack.push([node.left, depth + 1]);
+  }
+
+  return minDepth;
+};
+
+module.exports = minDepth;
