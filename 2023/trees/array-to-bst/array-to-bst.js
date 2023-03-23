d11347e1828dab0ba46f863103d080e681ba5ba4
Kirill Shakirov <kirillshakirov57@gmail.com>
2023-03-23 12:02:23 +0530

    task: array to bst

---
+++ 2023/trees/array-to-bst/array-to-bst.js
@@ -0,0 +1,11 @@
+const arrayToBST = (arr) => {
+  if (!arr.length) return null;
+
+  const inner = (arr, subtree) => {
+    if (!arr.length) return subtree;
+
+    const p = Math.floor(arr.length / 2);
+  };
+};
+
+module.exports = arrayToBST;
