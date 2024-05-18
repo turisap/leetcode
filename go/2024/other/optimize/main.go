60dcd58618d74f4a92a6e7f4d79753d9ab300e03
Kirill Shakirov <kirillshakirov57@gmail.com>
2024-05-18 10:56:43 +0700

    task: optimize layout

---
+++ go/2024/other/optimize/main.go
@@ -0,0 +1,7 @@
+package main
+
+
+func optRecursive() {}
+
+
+func optIterative() {}
---
+++ go/2024/other/optimize/test_cases.go
@@ -0,0 +1,8 @@
+package main
+var testCases = []struct {
+                                         		name   string
+                                         		s      string
+                                         		result int
+                                         	}{
+                                         		{"II", "II", 2},
+                                         	}
