87a2aa4420590c73a4da929203c9853859607265
Kirill Shakirov <kirillshakirov57@gmail.com>
2024-05-21 16:01:23 +0700

    task: extended naive solution

--- go/2024/other/optimize/s1.go
+++ go/2024/other/optimize/s1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
@@ -33,31 +34,66 @@ method on an interface and on a concrete struct
 */
 
 /* What is needed?
-a map
-a value struct
-a slice
-some functions to inline
+DONE a map
+DONE  a value struct
+DONE a slice
+DONE some functions to inline
 defers
-a global (some kind of average)
-a for range
-another map cycle for access
-another map for keys (maybe a part of them)
-some function which accepts interfaces
+DONE a global (some kind of average)
+DONE a for range to replace with for i
+DONE another map cycle for access
+DONE another map for keys (maybe a part of them)
+DONE some function which accepts interfaces
 */
 
-type Measure struct {
-	temp   float64
-	time   time.Time
-	active bool
-	source string
+type Measure interface {
+	getTemperature() float64
+	getSource() string
 }
 
-var avg float64 = 0.0
+type Celsius struct {
+	temp      float64
+	time      time.Time
+	active    bool
+	source    string
+	footprint float64
+}
 
-func s1(f *os.File) float64 {
+func (c Celsius) getTemperature() float64 {
+	return c.temp
+}
+
+func (c Celsius) getSource() string {
+	return c.source
+}
+
+type Fahrenheit struct {
+	temp      float64
+	time      time.Time
+	active    bool
+	source    string
+	footprint float64
+}
+
+func (f Fahrenheit) getTemperature() float64 {
+	return f.temp
+}
+
+func (f Fahrenheit) getSource() string {
+	return f.source
+}
+
+var sum float64 = 0.0
+var maxTemp float64 = 0.0
+var minTemp float64 = 0.0
+var fKoeff = 0.0
+
+func s1(f *os.File) (float64, float64, float64, float64) {
 	scanner := bufio.NewScanner(f)
-	measuresMap := map[string]Measure{}
-	keysMap := map[string]bool{}
+	fMap := map[string]Fahrenheit{}
+	cMap := map[string]Celsius{}
+	// @TODO pre-alloc
+	targetSources := []string{}
 
 	for scanner.Scan() {
 		ln := scanner.Text()
@@ -74,8 +110,11 @@ func s1(f *os.File) float64 {
 			continue
 		}
 
-		keysMap[source] = true
-		measuresMap[source] = Measure{
+		if isTargetSource(source) {
+			targetSources = append(targetSources, source)
+		}
+
+		fMap[source] = Fahrenheit{
 			temp:   temperature,
 			time:   time.Now(),
 			active: false,
@@ -83,5 +122,82 @@ func s1(f *os.File) float64 {
 		}
 	}
 
-	return 0.0
+	// map to celsius @TODO (pre-alloc map)
+	for _, v := range fMap {
+		m := fToCelsius(v)
+		c, ok := m.(Celsius)
+
+		if !ok {
+			continue
+		}
+
+		cMap[c.source] = c
+
+	}
+
+	// @TODO for range keys access temperatures
+	// calculate avg, min, max
+	//
+
+	// @TODO replace with for i
+	for i, v := range targetSources {
+		curr := cMap[v].temp
+
+		if curr < minTemp {
+			// @TODO global to local
+			minTemp = curr
+		}
+
+		if curr > maxTemp {
+			// @TODO global to local
+			maxTemp = curr
+		}
+
+		if i%17 == 0 {
+			fKoeff = calcFloatSth(curr) // complex float manipulations
+		}
+
+		sum += curr
+	}
+
+	return sum / float64(len(targetSources)), minTemp, maxTemp, fKoeff
+}
+
+// @TODO manual inline
+func calcFloatSth(f float64) float64 {
+	a := f + 3.4
+	b := f - 20.32
+	k := f * 3.1 / 4.1
+
+	return 3.0*a + 5.2*b - k*2.2
+}
+
+// @TODO call a method on concrete type, not on an interface
+func fToCelsius(m Measure) Measure {
+	// @TODO assign repetitive cals to getTemperature to a var
+	return Celsius{
+		temp:      degreesFtoCelsius(m.getTemperature()),
+		source:    m.getSource(),
+		footprint: calcFootprint(m.getTemperature()),
+	}
+}
+
+// @TODO to inline
+// @TODO float to ints
+func degreesFtoCelsius(f float64) float64 {
+	return (f - 32) * 0.55
+}
+
+func calcFootprint(f float64) float64 {
+	e := f * 2.0
+	k := f / 3.87
+
+	a := 7*e + 3*k
+
+	return a / 5
+}
+
+// @TODO inline
+func isTargetSource(s string) bool {
+	return rand.Intn(2) == 1
 }
