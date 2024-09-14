6d27a09725c247f34db15ad419e2972c90f7adf2
Kirill Shakirov <kirillshakirov57@gmail.com>
2024-09-14 18:55:25 +0700

    task: pipeline with cancel

--- go/2024/lang/autumn-warmup/pipe/main.go
+++ go/2024/lang/autumn-warmup/pipe/main.go
@@ -1,12 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func getSrcChan() <-chan int {
+type Done chan struct{}
+
+func getSrcChan(done Done) <-chan int {
 	out := make(chan int)
+
 	go func() {
-		for v := range 10 {
-			out <- v
+		defer func() {
+			fmt.Println("exit  out")
+			close(out)
+		}()
+
+		for v := range 100 {
+			select {
+			case out <- v:
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	return out
+}
+
+func sq(in <-chan int, done Done) <-chan int {
+	out := make(chan int)
+
+	go func() {
+		defer func() {
+			fmt.Println("exit  sq")
+			close(out)
+		}()
+		for v := range in {
+			select {
+			case out <- v * v:
+			case <-done:
+				return
+			}
 		}
 		close(out)
 	}()
@@ -14,24 +49,20 @@ func getSrcChan() <-chan int {
 	return out
 }
 
-func sq(in <-chan int) <-chan int {
-	out := make(chan int)
-	go func() {
-		for v := range in {
-			out <- v * v
-		}
-		close(out)
-	}()
-
-	return out
-}
-
-func dv(in <-chan int) <-chan int {
+func dv(in <-chan int, done Done) <-chan int {
 	out := make(chan int)
 
 	go func() {
+		defer func() {
+			fmt.Println("exit  dv")
+			close(out)
+		}()
 		for v := range in {
-			out <- v / 2
+			select {
+			case out <- v * 2:
+			case <-done:
+				return
+			}
 		}
 		close(out)
 	}()
@@ -40,10 +71,17 @@ func dv(in <-chan int) <-chan int {
 }
 
 func main() {
+	done := make(Done)
 
-	res := dv(sq(getSrcChan()))
+	res := dv(sq(getSrcChan(done), done), done)
+
+	go func() {
+		time.Sleep(time.Second)
+		close(done)
+	}()
 
 	for m := range res {
+		time.Sleep(time.Millisecond * 100)
 		fmt.Println(m)
 	}
 }
