badab8c65fea881272713b89399292e3a08476d5
Kirill Shakirov <kirillshakirov57@gmail.com>
2024-10-08 15:03:25 +0700

    task: max depth iterative

--- go/2024/autumn-warmup/trees/max-depth/max-d.go
+++ go/2024/autumn-warmup/trees/max-depth/max-d.go
@@ -1,6 +1,8 @@
 package max_depth
 
-import "math"
+import (
+	"math"
+)
 
 type TreeNode struct {
 	Val   int
@@ -8,12 +10,62 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func maxDepth(root *TreeNode) int {
+type Element struct {
+	Node  *TreeNode
+	Depth int
+}
+
+type Queue struct {
+	els []Element
+}
+
+func (q *Queue) Len() int {
+	return len(q.els)
+}
+
+func (q *Queue) Push(element Element) {
+	q.els = append(q.els, element)
+}
+
+func (q *Queue) Pop() Element {
+	el := q.els[len(q.els)-1]
+	q.els = q.els[0 : len(q.els)-1]
+
+	return el
+}
+
+func maxDepthRecursive(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
-	ld := maxDepth(root.Left) + 1
-	rd := maxDepth(root.Right) + 1
+	ld := maxDepthRecursive(root.Left) + 1
+	rd := maxDepthRecursive(root.Right) + 1
 
 	return int(math.Max(float64(ld), float64(rd)))
+}
+
+func maxDepthIterative(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	q := Queue{}
+	q.Push(Element{root, 1})
+	m := 0
+
+	for q.Len() > 0 {
+		curr := q.Pop()
+
+		if curr.Depth > m {
+			m = curr.Depth
+		}
+
+		if curr.Node.Left != nil {
+			q.Push(Element{curr.Node.Left, curr.Depth + 1})
+		}
+		if curr.Node.Right != nil {
+			q.Push(Element{curr.Node.Right, curr.Depth + 1})
+		}
+	}
+
+	return m
 }
